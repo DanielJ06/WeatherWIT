4245807ee71cd91e6022cad05d7357c978817f29
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-11-09 15:12:52 -0300

    Add search your own city input

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,16 +1,29 @@
+import { useNavigation } from '@react-navigation/core';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React, { useEffect, useState } from 'react';
 import { Dimensions, FlatList, Image, Text, View } from 'react-native';
 import CityCard from '../../components/CityCard';
+import Input from '../../components/Input';
 
 import { Cities } from '../../utils/citiesList';
 
 const Main: React.FC = () => {
+
+	const navigation = useNavigation<NativeStackNavigationProp<any,any>>();
+	const [cityName, setCityName] = useState("");
+
 	return (
 		<View style={{ flex: 1, alignItems: 'center', backgroundColor: '#FFF' }} >
 			<FlatList
 				ListHeaderComponent={
 					<>
 						<Text>Hello, user!</Text>
+						<Input 
+							btnAction={() => navigation.navigate('WeatherInfo', { cityName: cityName.trim().replace(/ /g, '%20') })}
+							disabled={!cityName}
+							value={cityName}
+							onChangeText={setCityName}
+						/>
 					</>
 				}
 				style={{ marginTop: 12, marginBottom: 12 }}

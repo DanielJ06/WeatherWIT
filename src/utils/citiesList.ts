994691eb3515a10bae1565a722a3b7c7832c9235
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-11-09 15:14:02 -0300

    Fake cities list

---
+++ src/utils/citiesList.ts
@@ -0,0 +1,63 @@
+// Lisboa, Madrid, Paris, Berlim, Copenhaga, Roma, Londres, Dublin, Praga e Viena.
+export const Cities = [
+	{
+		id: "1",
+		name: "Lisboa",
+		country: "PT - Portugal",
+		cover: "https://images.unsplash.com/photo-1571767750274-4795bdfcb642?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=327&q=80"
+	},
+	{
+		id: "2",
+		name: "Madrid",
+		country: "ES - Espanha",
+		cover: "https://images.unsplash.com/photo-1578305698944-874fa44d04c9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bWFkcmlkfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+	},
+	{
+		id: "3",
+		name: "Paris",
+		country: "FR - França",
+		cover: "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+	},
+	{
+		id: "4",
+		name: "Berlim",
+		country: "DE - Alemanha",
+		cover: "https://images.unsplash.com/photo-1560969184-10fe8719e047?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
+	},
+	{
+		id: "5",
+		name: "Copenhaga",
+		country: "DK - Dinamarca",
+		cover: "https://images.unsplash.com/photo-1552560880-2482cef14240?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29wZW5oYWdlbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+	},
+	{
+		id: "6",
+		name: "Roma",
+		country: "IT - Itália",
+		cover: "https://images.unsplash.com/photo-1537799943037-f5da89a65689?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1372&q=80"
+	},
+	{
+		id: "7",
+		name: "Londres",
+		country: "GB - Reino Unido",
+		cover: "https://images.unsplash.com/photo-1500380804539-4e1e8c1e7118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
+	},
+	{
+		id: "8",
+		name: "Dublin",
+		country: "IE - Irlanda",
+		cover: "https://images.unsplash.com/photo-1589327934270-659692dd1d28?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
+	},
+	{
+		id: "9",
+		name: "Praga",
+		country: "CZ - República Checa",
+		cover: "https://images.unsplash.com/photo-1519677100203-a0e668c92439?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
+	},
+	{
+		id: "10",
+		name: "Viena",
+		country: "AT - Áustria",
+		cover: "https://images.unsplash.com/photo-1573167443175-867d91708f97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=381&q=80"
+	},
+]
